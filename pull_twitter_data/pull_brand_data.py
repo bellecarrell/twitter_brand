18c3e5dbf85436a9189a5fa0fda5dfe9da962d9d
Adrian Benton <adrian.benton@gmail.com>
2018-08-25 22:50:15 -0400

    add script to pull brand data for all users

---
+++ pull_twitter_data/pull_brand_data.py
@@ -0,0 +1,64 @@
+'''
+Pulls data for brand-like, self-promoting users.
+
+Adrian Benton
+8/25/2018
+'''
+
+import os, re, time
+
+TOP_DIR = '/export/b08/abenton/twitter_brand_data/'
+#if not os.path.exists(TOP_DIR): os.mkdir(TOP_DIR)
+
+KEY = 'dl1'
+KEY_PATH = '/export/b08/abenton/twitter_collection/access_tokens/keys_{}.txt'.format(KEY)
+ACCESS_TOKEN_PATH = '/export/b08/abenton/twitter_collection/access_tokens/access_token_{}.txt'.format(KEY)
+
+def getTimeString():
+  t = time.localtime()
+  return '%d-%d-%d-%d-%d-%d' % (t.tm_year, t.tm_mon,
+                                t.tm_mday, t.tm_hour,
+                                t.tm_min, t.tm_sec)
+
+def make_dirs(job):
+  job_dir = os.path.join(TOP_DIR, job)
+  if not os.path.exists(job_dir):
+    os.mkdir(job_dir)
+  
+  time_stamp = time.time()
+  time_dir = os.path.join(job_dir, time_stamp):
+  if not os.path.exists(time_dir):
+    os.mkdir(time_dir)
+  
+  info_dir = os.path.join(time_dir, 'user_information')
+  tweet_dir = os.path.join(time_dir, 'user_timeline')
+  #network_dir = os.path.join(time_dir, 'networks')
+
+  for d in [info_dir, tweet_dir]:
+    if not os.path.exists(d):
+      os.mkdir(d)
+
+  return time_dir
+
+def pull_data(kw_path, time_dir):
+  info_dir = os.path.join(time_dir, 'user_information')
+  tweet_dir = os.path.join(time_dir, 'user_timeline')
+
+  os.system('python twitter_search.py get_user_information --accesstoken {} --keypath {} --kwpath {} --outdir {}'.format(ACCESS_TOKEN_PATH, KEY_PATH, kw_path, info_dir))
+  
+  os.system('python twitter_search.py user_timeline --accesstoken {} --keypath {} --kwpath {} --outdir {} --numtocache 3200'.format(ACCESS_TOKEN_PATH, KEY_PATH, kw_path, tweet_dir))
+
+BRAND_USER_DIR = '/export/b08/abenton/brand_users/'
+
+brand_user_re = re.compile('(?P<job>\w+)\.users.txt')
+
+for p in os.listdir(BRAND_USER_DIR):
+  m = brand_user_re.match(p)
+  if m:
+    job = m.group('job')
+  else:
+    continue
+  
+  time_dir = make_dirs(job)
+  pull_data(os.path.join(BRAND_USER_DIR, p), time_dir)
+  
