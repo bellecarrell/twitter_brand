dae307834afb550f20a60f8ce14fea289e30caaf
Adrian Benton <adrian.benton@gmail.com>
2018-09-09 17:44:43 -0400

    added method to pull past tweets in parallel, added cron job example

--- pull_twitter_data/pull_brand_data.py
+++ pull_twitter_data/pull_brand_data.py
@@ -1,3 +1,4 @@
+
 '''
 Pulls data for brand-like, self-promoting users.
 
@@ -6,7 +7,9 @@ Adrian Benton
 '''
 #!/usr/bin/env python
 
-import os, re, time
+import argparse
+import os, random, re, time
+from multiprocessing import Process
 
 #TOP_DIR = '/exp/abenton/twitter_brand_data/'
 TOP_DIR = '/exp/acarrell/twitter_brand/user_infos/'
@@ -19,6 +22,17 @@ ACCESS_TOKEN_PATH = '/exp/abenton/twitter_collection/access_tokens/access_token_
 BRAND_USER_DIR = '/exp/acarrell/twitter_brand/blogger_2018/17_allids/'
 
 brand_user_re = re.compile('(?P<job>\w+)\.users\.txt')
+
+WORKING_KEYS = ['ac', 'dl1', 'dl2', 'dl3', 'dl4', 'home', 't1', 't2', 't3', 't4',
+                't5', 't6', 't7', 't8', 'tv2', 'tv3', 'tv4', 'tv5', 'tv6']
+WORKING_KEY_PATHS = ['/exp/abenton/twitter_collection/keys/{}_keys.txt'.format(key)
+                     for key in WORKING_KEYS
+]
+WORKING_ACCESS_TOKEN_PATHS = [
+  '/exp/abenton/twitter_collection/access_tokens/access_token_{}.txt'.format(key)
+  for key in WORKING_KEYS
+]
+N_KEYS = len(WORKING_KEYS)
 
 def getTimeString():
   t = time.localtime()
@@ -46,13 +60,43 @@ def make_dirs(job):
   
   return job_dir
 
-def pull_data(kw_path, job_dir):
+def pull_data(kw_path, job_dir, method):
   info_dir = os.path.join(job_dir, 'user_information')
   tweet_dir = os.path.join(job_dir, 'user_timeline')
+
+  if method == 'userinfo':
+    os.system('python twitter_search.py get_user_information --accesstoken {} --keypath {} --kwpath {} --outdir {}'.format(ACCESS_TOKEN_PATH, KEY_PATH, kw_path, info_dir))
   
-  os.system('python twitter_search.py get_user_information --accesstoken {} --keypath {} --kwpath {} --outdir {}'.format(ACCESS_TOKEN_PATH, KEY_PATH, kw_path, info_dir))
-  
-  #os.system('python twitter_search.py user_timeline --accesstoken {} --keypath {} --kwpath {} --outdir {} --numtocache 200'.format(ACCESS_TOKEN_PATH, KEY_PATH, kw_path, tweet_dir))
+  elif method == 'pasttweets':
+    # Split up user lists into N different files on-the-fly and pull past tweets
+    f = open(kw_path, 'rt')
+    elts = f.readlines()
+    f.close()
+    random.shuffle(elts)
+
+    split_paths = []
+    for i in range(N_KEYS):
+      out_kw_path = '{}.{}'.format(kw_path, i)
+      f = open(out_kw_path, 'wt')
+      for j, elt in enumerate(elts):
+        if (j % N_KEYS) == i:
+          f.write(elt)
+      f.close()
+      split_paths.append(out_kw_path)
+    
+    ps = [Process(
+      target=lambda apath, kpath, kwpath: os.system('python twitter_search.py user_timeline --accesstoken {} --keypath {} --kwpath {} --outdir {} --numtocache 3200'.format(
+        apath, kpath, kwpath, tweet_dir)),
+      args=(apath, kpath, kwpath,)) for apath, kpath, kwpath in
+          zip(WORKING_ACCESS_TOKEN_PATHS, WORKING_KEY_PATHS, split_paths)]
+    for p in ps:
+      p.start()
+    for p in ps:
+      p.join()
+    
+    #os.system('python twitter_search.py user_timeline --accesstoken {} --keypath {} --kwpath {} --outdir {} --numtocache 3200'.format(ACCESS_TOKEN_PATH, KEY_PATH, kw_path, tweet_dir))
+  else:
+    raise Exception('Do not recognize method "{}"'.format(method))
 
 for p in os.listdir(BRAND_USER_DIR):
   m = brand_user_re.match(p)
@@ -61,6 +105,14 @@ for p in os.listdir(BRAND_USER_DIR):
   else:
     continue
   
+  parser = argparse.ArgumentParser(
+    description='Pull brand data, either user information or past tweets')
+  parser.add_argument('method', metavar='METHOD', choices=['userinfo',
+                                                           'pasttweets'],
+                      help='type of data to collect, "pasttweets" should ' +
+                           'run much less frequently than user info')
+  args = parser.parse_args()
+  
   job_dir = make_dirs(job)
-  pull_data(os.path.join(BRAND_USER_DIR, p), job_dir)
+  pull_data(os.path.join(BRAND_USER_DIR, p), job_dir, args.method)
   print('Finished pulling data for "{}" users'.format(job))
--- pull_twitter_data/pull_brand_data.sh
+++ pull_twitter_data/pull_brand_data.sh
@@ -1,3 +1,3 @@
 cd /home/hltcoe/acarrell/PycharmProjects/twitter_brand/pull_twitter_data/
-python /home/hltcoe/acarrell/PycharmProjects/twitter_brand/pull_twitter_data/pull_brand_data.py
+python /home/hltcoe/acarrell/PycharmProjects/twitter_brand/pull_twitter_data/pull_brand_data.py ${1}
 cd -
