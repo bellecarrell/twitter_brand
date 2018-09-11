1217c447883466042ffbc8a2b442be56b368bf05
Adrian Benton <adrian.benton@gmail.com>
2018-09-11 06:42:22 -0400

    activate python 3

--- pull_twitter_data/pull_brand_data.sh
+++ pull_twitter_data/pull_brand_data.sh
@@ -1,3 +1,4 @@
 cd /home/hltcoe/acarrell/PycharmProjects/twitter_brand/pull_twitter_data/
+source activate py3k
 python /home/hltcoe/acarrell/PycharmProjects/twitter_brand/pull_twitter_data/pull_brand_data.py ${1}
 cd -
