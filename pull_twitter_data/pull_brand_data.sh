fcb5dbb1055675108e7c5bf4ec0515f9eda13f89
Annabelle <belle.carrell@gmail.com>
2018-09-11 23:31:55 -0400

    Modify source activate line to include full path

--- pull_twitter_data/pull_brand_data.sh
+++ pull_twitter_data/pull_brand_data.sh
@@ -1,4 +1,4 @@
 cd /home/hltcoe/acarrell/PycharmProjects/twitter_brand/pull_twitter_data/
-source activate py3k
+source activate /home/hltcoe/abenton/.conda/envs/py3k
 python /home/hltcoe/acarrell/PycharmProjects/twitter_brand/pull_twitter_data/pull_brand_data.py ${1}
 cd -
