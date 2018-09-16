95eb592c381dfea3dcf1f844fce56a37f402a6df
Annabelle <belle.carrell@gmail.com>
2018-09-16 14:52:56 -0400

    Change sourced env to be in acarrell home directory

--- pull_twitter_data/pull_brand_data.sh
+++ pull_twitter_data/pull_brand_data.sh
@@ -1,5 +1,6 @@
 cd /home/hltcoe/acarrell/PycharmProjects/twitter_brand/pull_twitter_data/
-export PATH=/opt/anaconda2/bin/:${PATH}
-source activate py3k
+#export PATH=/opt/anaconda2/bin/:${PATH}
+#source activate py3k
+source activate /home/hltcoe/acarrell/.conda/envs/py3
 python /home/hltcoe/acarrell/PycharmProjects/twitter_brand/pull_twitter_data/pull_brand_data.py ${1}
 cd -
