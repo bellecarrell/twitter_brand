2e0d34df44e0ee5b34d3f8de1a85c38ef60c7867
Adrian Benton <abenton10@bloomberg.net>
2018-11-25 17:35:49 -0500

    developin

--- analysis/tweets_over_time.py
+++ analysis/tweets_over_time.py
@@ -10,10 +10,11 @@ import datetime
 import pandas as pd
 
 
-def main(tweet_path, user_label_path):
+def main(tweet_path, user_label_path, out_path):
     tweet_df = pd.read_table(tweet_path, sep=',')
     user_df = pd.read_table(user_label_path)
-    
+
+    # restrict to self-promoting
     prom_user_df = user_df[user_df['classify_account-mace_label']=='promoting']
     promoting_users = set(prom_user_df['user_id'].tolist())
     
@@ -26,10 +27,53 @@ def main(tweet_path, user_label_path):
     filt_tweet_df['main_specialization'] = [u_to_lab[u]
                                             for u
                                             in filt_tweet_df['user_id']]
-    
-    import pdb; pdb.set_trace()
-    
 
+    # map unix timestamps to datetimes
+    filt_tweet_df['datetime'] = filt_tweet_df['created_at'].map(
+        lambda x: datetime.datetime.fromtimestamp(x)
+    )
+    filt_tweet_df['month'] = filt_tweet_df['datetime'].map(
+        lambda x: (x.year, x.month)
+    )
+    
+    print('Tweets collected from "{}" to "{}"'.format(
+        filt_tweet_df['datetime'].min(), filt_tweet_df['datetime'].max())
+    )
+
+    # Total tweets per month
+    t_per_mon = filt_tweet_df.groupby('month')['user_id'].count()
+    
+    # Unique users per month
+    u_per_mon = filt_tweet_df.groupby('month')['user_id'].unique().map(len)
+
+    all_mon_idx = u_per_mon.index
+
+    # tweets per user per month
+    counts_per_user = {}
+    for u in filt_tweet_df['user_id'].unique():
+        u_tweet_subset = filt_tweet_df[filt_tweet_df['user_id']==u]
+        counts_per_user[u] = \
+            u_tweet_subset.groupby('month')['user_id'].count().reindex(
+                u_per_mon.index, fill_value=0
+            )
+
+    # users per specialization per month
+    counts_per_spec = {}
+    for s in filt_tweet_df['main_specialization'].unique():
+        s_tweet_subset = filt_tweet_df[filt_tweet_df['main_specialization']==s]
+        counts_per_spec[s] = \
+            s_tweet_subset.groupby('month')['user_id'].unique().map(len).reindex(
+                u_per_mon.index, fill_value=0
+            )
+    
+    df = pd.DataFrame(dict(list(counts_per_spec.items()) +
+                           list(counts_per_user.items()) +
+                           [('unique_users', u_per_mon),
+                            ('num_tweets', t_per_mon)]))
+    
+    df.to_csv(out_path, sep='\t', index=False, header=True)
+
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
       description='count tweets made over time'
@@ -38,6 +82,8 @@ if __name__ == '__main__':
                         help='path with all tweets')
     parser.add_argument('--user_label_path', required=True,
                         help='path to MTurk-labeled users')
+    parser.add_argument('--out_path', required=True,
+                        help='table of counts per month')
     args = parser.parse_args()
     
-    main(args.tweet_path, args.user_label_path)
+    main(args.tweet_path, args.user_label_path, args.out_path)
