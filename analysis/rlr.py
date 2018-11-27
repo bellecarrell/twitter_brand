e1dae07050978db2f7edb6f83e8ec95c044e165b
Adrian Benton <abenton10@bloomberg.net>
2018-11-27 10:58:01 -0500

    support sparse weights; count +/-/abs.; add min threshold

--- analysis/rlr.py
+++ analysis/rlr.py
@@ -16,7 +16,7 @@ SEED = 12345
 VERBOSE = False
 
 class RandomizedRegression:
-    def __init__(self, is_continuous=False, model_dir=None, log_l1_range=False, count_absolute_threshold=None):
+    def __init__(self, is_continuous=False, model_dir=None, log_l1_range=False, min_absolute_threshold=0.0):
         self.is_continuous = is_continuous  # fit linear regression instead of logistic
         self.model_dir = model_dir  # where to save model weights
         self.log_l1_range = log_l1_range  # pick l1 logarithmically from random
@@ -28,15 +28,27 @@ class RandomizedRegression:
         else:
             self._build_model = lambda l1: sklearn.linear_model.LogisticRegression(penalty='l1', C=l1)
         
-        self.count_absolute_threshold = count_absolute_threshold
-        self.feature_counts = {}
+        self.min_absolute_threshold = min_absolute_threshold
+        
+        self.neg_feature_counts = {}
+        self.pos_feature_counts = {}
+        self.abs_feature_counts = {}
     
-    def get_salient_features(self, feature_key, target_key, n=100):
-        if not self.feature_counts:
+    def get_salient_features(self, feature_key, target_key, n=100, salience_type='pos'):
+        if not self.abs_feature_counts:
             print('Need to fit model first')
             return None
         else:
-            rev_cnts = [[(v, feature_key[k]) for k, v in self.feature_counts[i].items()]
+            if salience_type == 'pos':
+                feature_counts = self.pos_feature_counts
+            elif salience_type == 'neg':
+                feature_counts = self.neg_feature_counts
+            elif salience_type == 'abs':
+                feature_counts = self.abs_feature_counts
+            else:
+                raise Exception('Do not recognize count type: "{}"'.format(salience_type))
+
+            rev_cnts = [[(v, feature_key[k]) for k, v in feature_counts[i].items()]
                         for i, target in target_key.items()]
             for cnts in rev_cnts:
                 cnts.sort(reverse=True)
@@ -90,7 +102,11 @@ class RandomizedRegression:
         
         model.fit(X, y)
         
-        wts = model.coef_
+        if 'sparse_coef_' in dir(model):
+            wts = model.sparse_coef_
+        else:
+            model = model.sparsify()
+            wts   = model.coef_
         
         if self.model_dir is not None:
             if not os.path.exists(self.model_dir):
@@ -101,26 +117,52 @@ class RandomizedRegression:
             with gzip.open(out_path, 'w') as model_file:
                 pickle.dump(model, model_file)
         
-        # count which features are positive, or have large absolute value
+        # count which features are positive, negative, or have large absolute value
         if len(wts.shape) == 1:
-            if 0 not in self.feature_counts:
-                self.feature_counts[0] = {}
+            if 0 not in self.abs_feature_counts:
+                self.pos_feature_counts[0] = {}
+                self.neg_feature_counts[0] = {}
+                self.abs_feature_counts[0] = {}
             
-            for i, v in enumerate(wts):
-                if (self.count_absolute_threshold and np.abs(v) > self.count_absolute_threshold) or (not self.count_absolute_threshold and (v > 0)):
-                    if i not in self.feature_counts[0]:
-                        self.feature_counts[0][i] = 0
-                    self.feature_counts[0][i] += 1
+            nz_idxes = wts.nonzero()[0]
+            
+            for i in nz_idxes:
+                v = wts[i]
+                
+                if i not in self.abs_feature_counts[0]:
+                    self.abs_feature_counts[0][i] = 0
+                    self.pos_feature_counts[0][i] = 0
+                    self.neg_feature_counts[0][i] = 0
+
+                if np.abs(v) > self.min_absolute_threshold:
+                    self.abs_feature_counts[0][i] += 1
+                if v > self.min_absolute_threshold:
+                    self.pos_feature_counts[0][i] += 1
+                if v < -self.min_absolute_threshold:
+                    self.neg_feature_counts[0][i] += 1
         else:
             for target in range(wts.shape[0]):
-                if target not in self.feature_counts:
-                    self.feature_counts[target] = {}
+                if target not in self.abs_feature_counts:
+                    self.pos_feature_counts[target] = {}
+                    self.neg_feature_counts[target] = {}
+                    self.abs_feature_counts[target] = {}
+
+                nz_idxes = wts[target].nonzero()[1]
                 
-                for i, v in enumerate(wts[target]):
-                    if (self.count_absolute_threshold and np.abs(v) > self.count_absolute_threshold) or (not self.count_absolute_threshold and (v > 0)):
-                        if i not in self.feature_counts[target]:
-                            self.feature_counts[target][i] = 0
-                        self.feature_counts[target][i] += 1
+                for i in nz_idxes:
+                    v = wts[target, i]
+                    
+                    if i not in self.abs_feature_counts[0]:
+                        self.abs_feature_counts[target][i] = 0
+                        self.pos_feature_counts[target][i] = 0
+                        self.neg_feature_counts[target][i] = 0
+    
+                    if np.abs(v) > self.min_absolute_threshold:
+                        self.abs_feature_counts[target][i] += 1
+                    if v > self.min_absolute_threshold:
+                        self.pos_feature_counts[target][i] += 1
+                    if v < -self.min_absolute_threshold:
+                        self.neg_feature_counts[target][i] += 1
 
 
 def gen_data(n=200, p=200, noise=0.01):
@@ -153,7 +195,7 @@ def test_continuous():
     salient_features = lin_reg.get_salient_features({i: 'X{}'.format(i) for i in range(200)}, {0: 'target'}, n=5)
     
     # should be first 2 predictors more often than not
-    print('Salient features (should be X0 & X1): {}'.format(salient_features))
+    print('+ Salient features (should be X0 & X1): {}'.format(salient_features))
 
 
 def test_discrete():
@@ -167,7 +209,7 @@ def test_discrete():
     salient_features = log_reg.get_salient_features({i: 'X{}'.format(i) for i in range(200)}, {0: 'target'}, n=5)
 
     # should be first 2 predictors more often than not
-    print('Salient features (should be X0 & X1): {}'.format(salient_features))
+    print('+ Salient features (should be X0 & X1): {}'.format(salient_features))
 
 
 def main():
@@ -175,8 +217,5 @@ def main():
 
 
 if __name__ == '__main__':
-    
-    test_continuous()
-    
     test_discrete()
-    
+    test_continuous()
