33cf4e0459353db76f069bf61943fd80a70d14d2
belle.carrell <belle.carrell@gmail.com>
2018-12-02 22:12:17 -0500

    script for adding pc cols to static info

--- analysis/add_pc_cols_to_static_info.py
+++ analysis/add_pc_cols_to_static_info.py
@@ -11,32 +11,12 @@ import csv
 from collections import defaultdict
 
 def calculate_percent_change(static_info, in_dir, us):
-    info = gzip.open(os.path.join(in_dir, 'info/user_info_dynamic.tsv.gz'), 'rt')
+    df = pd.read_csv(os.path.join(in_dir, 'info/user_info_dynamic.tsv.gz'),sep='\t',compression='gzip')
     pcs = {}
     for u in us:
         start = static_info.loc[static_info['user_id'] == int(u)]['followers_count'].values[0]
-        max = 0
-        fc = 0
-        pcs[u] = [start,max,fc]
 
-
-    count = 0
-    reader = csv.reader(info)
-    for row in reader:
-        if count % 50000 == 0:
-            print('{} rows read from dynamic info file'.format(count))
-        if count == 0:
-            count += 1
-            continue
-        count += 1
-        row = row[0].split('\t')
-        timestamp, user_id, followers_count, friends_count, listed_count, statuses_count = row
-        timestamp, user_id, followers_count = int(timestamp), int(user_id), int(followers_count)
-        if user_id in us:
-            start, max, fc = pcs[u]
-            if timestamp > max:
                 pcs[u] = [start, timestamp, followers_count]
-                print('user: {} data: {}'.format(u, pcs[u]))
 
 
     for u in pcs.keys():
