f32f4e264ce8a85e641fb2d191d1fcc75242b683
Adrian Benton <abenton10@bloomberg.net>
2019-04-17 15:07:09 -0400

    fixed write out vocabulary

--- analysis/topic_and_sentiment/extract_tm_features.py
+++ analysis/topic_and_sentiment/extract_tm_features.py
@@ -3,8 +3,10 @@ Fit topic model to promoting user tweets and extract topic distribution per twee
 (compare NMF and LDA qualitatively, tuning hyperparameters for heldout perplexity).
 '''
 
-import os
+import json
 import multiprocessing as mp
+import numpy as np
+import os
 import pandas as pd
 
 from sklearn.decomposition import LatentDirichletAllocation, NMF
@@ -28,7 +30,16 @@ def fit_nmf(train_max, heldout_max=None, vocab=None, k=10, alpha_regularization=
     train_nmf = nmf.fit_transform(train_max)
     
     if heldout_max is not None:
-        heldout_max
+        heldout_nmf = nmf.transform(heldout_max)
+    
+    reconst_train_max = nmf.inverse_transform(train_nmf)
+    reconst_heldout_max = nmf.inverse_transform(heldout_nmf)
+    
+    prop_train_reconst_err   = np.linalg.norm(reconst_train_max - train_max) / np.linalg.norm(train_max)
+    prop_heldout_reconst_err = np.linalg.norm(reconst_heldout_max - heldout_max) / np.linalg.norm(heldout_max)
+    
+    print('Train reconstruction error: {}'.format(prop_train_reconst_err))
+    print('Heldout reconstruction error: {}'.format(prop_heldout_reconst_err))
     
     import pdb; pdb.set_trace()
 
@@ -37,18 +48,18 @@ def fit_lda(train_max, heldout_max=None, vocab=None, k=10, alpha=1.0, beta=10**-
     pass
 
 
-def fit_model(args):
+def fit_model(train_max, heldout_max, vocab, args):
     if args[0] == 'nmf':
-        fit_nmf(args[1], args[2], args[3], args[4])
+        fit_nmf(train_max, heldout_max, vocab, args[1], args[2])
     elif args[0] == 'lda':
-        fit_lda(args[1], args[2], args[3], args[4], args[5])
+        fit_lda(train_max, heldout_max, vocab, args[1], args[2], args[3])
     else:
         raise Exception('Do not recognize model "{}"'.format(args[0]))
-    
-    pass
 
 
 def main():
+    
+    # read in text matrix
     all_max = scipy.sparse.load_npz(IDF_FEATURE_PATH)
     
     if PROP_TRAIN < 1.0:
@@ -59,20 +70,24 @@ def main():
         train_max = all_max
         heldout_max = None
     
+    # read in vocabulary
+    with open(VOCAB_PATH, 'rt') as vocab_file:
+        json.load(vocab_file)
+    
     arg_lst = []
     
     for k in [10, 20, 50, 100]:
         # NMF runs
         for alpha_reg in [0.0, 1.0, 10.0]:
-            arg_lst.append(('nmf', train_max, heldout_max, k, alpha_reg))
+            arg_lst.append(('nmf', k, alpha_reg))
         
         # LDA runs
-        #arg_lst.append(('lda', train_max, heldout_max, k, 0.5, 10**-3))
+        arg_lst.append(('lda', k, 0.5, 10**-3))
     
     for i, args in enumerate(arg_lst):
         fit_model(args)
         
-        print('Finished {}/{}'.format(i+1, len(arg_lst)))
+        print('Finished {}/{} ({})'.format(i+1, len(arg_lst), args[0]))
 
 
 if __name__ == '__main__':
