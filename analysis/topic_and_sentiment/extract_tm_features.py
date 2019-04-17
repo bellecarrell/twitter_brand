32a3d7761caebac705d9f6504a26e56d1402f3dc
Adrian Benton <abenton10@bloomberg.net>
2019-04-17 16:21:22 -0400

    add lda

--- analysis/topic_and_sentiment/extract_tm_features.py
+++ analysis/topic_and_sentiment/extract_tm_features.py
@@ -27,10 +27,10 @@ VOCAB_PATH = os.path.join(WORKSPACE_DIR, 'vocab.json')
 TOPIC_DIR = os.path.join(WORKSPACE_DIR, 'topic_modeling')
 
 
-def get_top_words(nmf_model, vocab, n=10, verbose=False):
+def get_top_words(model, vocab, n=10, verbose=False):
     words_per_topic = []
     
-    for topic_idx, topic in enumerate(nmf_model.components_):
+    for topic_idx, topic in enumerate(model.components_):
         words = [vocab[i] for i in topic.argsort()[:-n - 1:-1]]
         words_per_topic.append(words)
         
@@ -75,17 +75,15 @@ def fit_nmf(train_max, heldout_max=None, vocab=None, k=10, alpha_regularization=
             prop_heldout_reconst_errs.append(prop_heldout_reconst_err)
         else:
             prop_heldout_reconst_errs.append(-1.0)
-        
-        print('Sampled reconstruction error iteration: {}/20'.format(iteration))
     
     print('Train reconstruction error: {}'.format(np.mean(prop_train_reconst_errs)))
     print('Heldout reconstruction error: {}'.format(np.mean(prop_heldout_reconst_errs)))
     
     top_words_per_topic = get_top_words(nmf, vocab, n=20, verbose=False)
     
-    topic_path = os.path.join(TOPIC_DIR, 'nmf-k{}-alpha{}.topics.txt')
-    topic_dist_path = os.path.join(TOPIC_DIR, 'nmf-k{}-alpha{}.topic_distribution_per_tweet.txt')
-    model_path = os.path.join(TOPIC_DIR, 'nmf-k{}-alpha{}.model.pickle')
+    topic_path = os.path.join(TOPIC_DIR, 'nmf-k{}-alpha{}.topics.txt'.format(k, alpha_regularization))
+    topic_dist_path = os.path.join(TOPIC_DIR, 'nmf-k{}-alpha{}.topic_distribution_per_tweet.txt'.format(k, alpha_regularization))
+    model_path = os.path.join(TOPIC_DIR, 'nmf-k{}-alpha{}.model.pickle'.format(k, alpha_regularization))
     
     # save top words per topic
     with open(topic_path, 'wt', encoding='utf8') as topic_file:
@@ -110,34 +108,77 @@ def fit_nmf(train_max, heldout_max=None, vocab=None, k=10, alpha_regularization=
 
 
 def fit_lda(train_max, heldout_max=None, vocab=None, k=10, alpha=1.0, beta=10**-3):
-    pass
+    lda = LatentDirichletAllocation(k, alpha, beta)
+    
+    train_topic_probs = lda.fit_transform(train_max)
 
-
-def fit_model(train_max, heldout_max, vocab, args):
-    if args[0] == 'nmf':
-        fit_nmf(train_max, heldout_max, vocab, args[1], args[2])
-    elif args[0] == 'lda':
-        fit_lda(train_max, heldout_max, vocab, args[1], args[2], args[3])
+    if heldout_max is not None:
+        heldout_topic_probs = lda.transform(heldout_max)
     else:
-        raise Exception('Do not recognize model "{}"'.format(args[0]))
+        heldout_topic_probs = None
+    
+    train_ppl_varlowerbound   = lda.perplexity(train_max)
+    heldout_ppl_varlowerbound = lda.perplexity(heldout_max)
+    
+    top_words_per_topic = get_top_words(lda, vocab, n=20, verbose=False)
+    
+    topic_path = os.path.join(TOPIC_DIR, 'lda-k{}-alpha{}-beta{}.topics.txt'.format(k, alpha, beta))
+    topic_dist_path = os.path.join(TOPIC_DIR,
+                                   'lda-k{}-alpha{}-beta{}.topic_distribution_per_tweet.txt'.format(k,
+                                                                                                    alpha,
+                                                                                                    beta))
+    model_path = os.path.join(TOPIC_DIR, 'nmf-k{}-alpha{}-beta{}.model.pickle'.format(k, alpha, beta))
+
+    # save top words per topic
+    with open(topic_path, 'wt', encoding='utf8') as topic_file:
+        for topic_idx, words in enumerate(top_words_per_topic):
+            topic_file.write('Topic #{}:'.format(topic_idx))
+            for w in words:
+                topic_file.write(' ')
+                topic_file.write(w)
+            topic_file.write('\n')
+
+    # save model
+    with open(model_path, 'wb') as model_file:
+        pickle.dump(lda, model_file)
+
+    # save topic activation for each tweet, compressed numpy format
+    if heldout_topic_probs is not None:
+        topic_probs = np.concatenate((train_topic_probs,
+                                      heldout_topic_probs),
+                                     axis=0)
+    else:
+        topic_probs = train_topic_probs
+
+    np.savez_compressed(topic_dist_path, topics_per_tweet=topic_probs)
+
+    pass
 
 
 def main():
     # read in text matrix
     all_max = scipy.sparse.load_npz(IDF_FEATURE_PATH)
+    onehot_max = all_max.clone()
+    onehot_max[onehot_max.nonzero()] = 1
     
     if PROP_TRAIN < 1.0:
         split_idx = int(PROP_TRAIN*all_max.shape[0])
         train_max = all_max[:split_idx, :]
         heldout_max = all_max[split_idx:, :]
+        
+        onehot_train_max   = onehot_max[:split_idx, :]
+        onehot_heldout_max = onehot_max[split_idx:, :]
     else:
         train_max = all_max
         heldout_max = None
+        
+        onehot_train_max   = onehot_max
+        onehot_heldout_max = None
     
     # read in vocabulary
     with open(VOCAB_PATH, 'rt') as vocab_file:
         vocab = json.load(vocab_file)
-    rev_vocab = {int(v): k for k, v in vocab.items()}
+        rev_vocab = {int(v): k for k, v in vocab.items()}
     
     arg_lst = []
     
@@ -151,9 +192,15 @@ def main():
     
     for i, args in enumerate(arg_lst):
         start = time.time()
-        fit_model(train_max, heldout_max, rev_vocab, args)
-        end = time.time()
         
+        if args[0] == 'nmf':
+            fit_nmf(train_max, heldout_max, vocab, args[1], args[2])
+        elif args[0] == 'lda':
+            fit_lda(onehot_train_max, onehot_heldout_max, rev_vocab, args[1], args[2], args[3])
+        else:
+            raise Exception('Do not recognize model "{}"'.format(args[0]))
+        
+        end = time.time()
         print('({}s) Finished {}/{} ({})'.format(int(end - start), i+1, len(arg_lst), args))
 
 
