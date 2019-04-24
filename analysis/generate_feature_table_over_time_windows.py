50822b28e5dc00532b6573fe9f75f02583d13df4
Adrian Benton <adrian.donald.benton@gmail.com>
2019-04-24 13:18:26 -0400

    Debug generating feature table (#112)
    * read_table
    
    * for debugging, promoting_users to singleton
    
    * 6pm - 6pm
    
    * tz
    
    * tz
    
    * wide table format
    
    * remove pdb, timeline file is incomplete
    
    * remove print statements, write header

--- analysis/generate_feature_table_over_time_windows.py
+++ analysis/generate_feature_table_over_time_windows.py
@@ -5,75 +5,97 @@ import datetime
 
 
 def main(in_dir, out_dir):
-    i = 0
-
-    print('spot {}'.format(i))
-    i +=1
     static_info = pd.read_csv(os.path.join(in_dir, 'static_info/static_user_info.csv'))
-    print('spot {}'.format(i))
-    i += 1
-    info = pd.read_csv(os.path.join(in_dir, 'info/user_info_dynamic.tsv.gz'),compression='gzip', sep='\t', error_bad_lines=False)
-    print('spot {}'.format(i))
-    i += 1
-    timeline = pd.read_csv(os.path.join(in_dir, 'timeline/user_tweets.noduplicates.tsv.gz'))
+    info = pd.read_table(os.path.join(in_dir, 'info/user_info_dynamic.tsv.gz'))
+    timeline = pd.read_table(os.path.join(in_dir, 'timeline/user_tweets.noduplicates.tsv.gz'))
     promoting_users = static_info.loc[
         static_info['classify_account-mace_label'] == 'promoting'
     ]['user_id'].dropna().unique().tolist()
-    print('spot {}'.format(i))
-    i += 1
-    promoting_users = promoting_users[0]
-
-    tws = [datetime.timedelta(days=1), datetime.timedelta(days=2), datetime.timedelta(days=3), datetime.timedelta(days=4), datetime.timedelta(days=5), datetime.timedelta(days=6),
-           datetime.timedelta(days=7), datetime.timedelta(weeks=2), datetime.timedelta(weeks=3), datetime.timedelta(weeks=4)]
-    dv_types = [('delta', 'followers_count'), ('percent','followers_count')]
+    promoting_users = [promoting_users[0]]
+    
+    tws = [datetime.timedelta(days=d) for d in [1, 2, 3, 4, 5, 6, 7, 14, 21, 28]]
+    dv_types = [('delta', 'followers_count'), ('percent', 'followers_count')]
     iv_types = [('average', 'rt')]
-
+    
+    EST = datetime.timezone(datetime.timedelta(hours=-5))
+    
     rows = []
+    
+    # AB: Refactored to write one row per <user_id, window_size_days, eval_date> triple.
+    # Easier to fit models with this format.
+    COLUMNS = ['user_id', 'window_size_days', 'window_start', 'window_stop', 'eval_date']
+    COLUMNS += ['IV_{}-{}'.format(iv_agg_name, iv_name) for iv_agg_name, iv_name in iv_types]
+    COLUMNS += ['DV_horizon{}_{}-{}'.format(horizon_width.days, dv_agg_name, dv_name)
+                for horizon_width in tws
+                for dv_agg_name, dv_name in dv_types]
+    
     for user in promoting_users:
+        u_infos = info.loc[info['user_id'] == user]
+        u_infos['datetime'] = u_infos['timestamp'].map(lambda x: datetime.datetime.fromtimestamp(x, tz=EST))
+        u_infos['date'] = u_infos['datetime'].map(lambda x: datetime.date(x.year, x.month, x.day))
+        
+        # AB: need to iterate over all dates that the user was active, not just the days on which they tweeted
         tweet_dates = timeline.loc[timeline['user_id']==user]['created_at'].unique().tolist()
+        min_tweet_ts = datetime.datetime.fromtimestamp(min(tweet_dates), tz=EST)
+        max_tweet_ts = datetime.datetime.fromtimestamp(max(tweet_dates), tz=EST)
+        
+        tweet_dates = []
+        
+        # compute features from 12pm - 12pm each day, Eastern time zone,
+        # since that is when we sampled the follower counts
+        curr_ts = datetime.datetime(min_tweet_ts.year,
+                                    min_tweet_ts.month,
+                                    min_tweet_ts.day,
+                                    12, 0, 0, 0,
+                                    EST)
+        while curr_ts < max_tweet_ts:
+            tweet_dates.append(curr_ts)
+            curr_ts += datetime.timedelta(days=1)
+        
         info_dates = info.loc[info['user_id']==user]['timestamp'].unique().tolist()
-
-        for date in tweet_dates:
+        max_info_date = max(info_dates)
+        
+        for date_idx, date in enumerate(tweet_dates):
             for i, window in enumerate(tws):
                 end = date + window
                 if end <= max(tweet_dates):
+                    row = [user, window.days, date, end, end]
+                    
                     # compute independent var values
                     for compute, column in iv_types:
-                        iv_vals = timeline.loc[(timeline['user_id'] == user) & (date <= timeline['created_at'] <= end)][column].tolist()
+                        iv_vals = timeline.loc[(timeline['user_id'] == user) &
+                                               (date <= timeline['created_at'] <= end)][column].tolist()
                         if compute == 'average':
                             iv_val = sum(iv_vals)/len(iv_vals)
-                        iv_type = compute + '_' + column
+                            row.append(iv_val)
+                        else:
+                            raise Exception('Do not recognize IV aggregation type: "{}"'.format(compute))
+                    
+                    # compute dependent vars for each horizon
+                    for j, horizon in enumerate(tws):
+                        h_start_day = datetime.date(end.year, end.month, end.day)
+                        h_end_day = h_start_day + horizon
+                        if datetime.datetime.timestamp(h_end_day) <= max_info_date:
+                            for compute, column in dv_types:
+                                c_start = u_infos.loc[u_infos['date'] == h_start_day][column]
+                                c_end = u_infos.loc[u_infos['date'] == h_end_day][column]
+                                
+                                # we sample 2x/day, take last sample as the change
+                                c_start = c_start.tolist()[-1]
+                                c_end = c_end.tolist()[-1]
+                                if compute == 'delta':
+                                    dv_val = c_end - c_start
+                                elif compute == 'percent':
+                                    dv_val = (c_end - c_start)/c_start
+                                else:
+                                    raise Exception('Do not recognize DV compute: "{}"'.format(compute))
+                                row.append(dv_val)
+                    
+                    rows.append(row)
 
-
-                        for j, horizon in enumerate(tws):
-                            h_start = end
-                            h_end = h_start + horizon
-                            if datetime.datetime.timestamp(h_end) <= max(info_dates):
-
-                                #compute dependent var values
-                                datetime.datetime.timestamp(h_start)
-                                u_infos = info.loc[info['user_id']==user]
-                                u_infos['date'] = datetime.date
-                                #todo: remove once date is added to user_infos table
-                                for index, row in u_infos.itterrows():
-                                    row['date'] = datetime.datetime.date(row['timestamp'])
-
-                                for compute, column in dv_types:
-                                    c_start = u_infos.loc[u_infos['date']==h_start][column]
-                                    c_end = u_infos.loc[u_infos['date'] == h_end][column]
-                                    if compute == 'delta':
-                                        dv_val = c_end - c_start
-                                    if compute == 'percent':
-                                        dv_val = (c_end - c_start)/c_start
-                                    dv_type = compute + '_' + column
-
-                                    row = [user, i, j, date, end, h_start, h_end, iv_type, iv_val, dv_type, dv_val]
-                                    rows.append(row)
-
-    ft = pd.DataFrame(rows, columns=['user_id','window_size','horizon_size', 'window_start',
-                               'window_stop', 'horizon_start','horizon_stop','iv_type','iv_value', 'dv_type','dv_value'])
-
-    ft.to_csv(os.path.join(out_dir,'feature_table.csv.gz'),compression='gzip')
+    ft = pd.DataFrame(rows, columns=COLUMNS)
+    
+    ft.to_csv(os.path.join(out_dir, 'feature_table.csv.gz'), compression='gzip')
 
 
 if __name__ == '__main__':
